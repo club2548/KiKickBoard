546194ded885f12d05900bbb73e647254ccfbabb
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-15 19:02:29 +0900

    gitignore추가 xcode프로젝트파일 추가

---
+++ KiKickBoard/KiKickBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KiKickBoard
+//
+//  Created by 원동진 on 2024/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
