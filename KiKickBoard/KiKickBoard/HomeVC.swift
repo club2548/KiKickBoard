f75ccbb82c07fd3f8e95f7565e256dfd9caded12
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-16 21:41:24 +0900

    UI 구조 설정

--- KiKickBoard/KiKickBoard/HomeVC.swift
+++ KiKickBoard/KiKickBoard/HomeVC.swift
@@ -18,18 +18,58 @@ class HomeVC: UIViewController{
         label.numberOfLines = 2
         return label
     }()
-    private lazy var mapView : KMViewContainer = {
-        let view = KMViewContainer()
+    private lazy var addressLabel : UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.text = "주소 입력"
+        label.textAlignment = .left
+        return label
+    }()
+    private lazy var addressStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        return stackView
+    }()
+    private lazy var addressTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "주소를 입력하세요."
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 10
+        textField.layer.masksToBounds = true
+        return textField
+    }()
+    private lazy var addressButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("주소 입력", for: .normal)
+        button.backgroundColor = .purple
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        return button
+    }()
+    private lazy var mapView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemCyan
         return view
     }()
-  
+    private lazy var rentalButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("대여하기", for: .normal)
+        button.backgroundColor = .purple
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationController?.navigationBar.topItem?.title = "Home"
-        
-        
-        
+        addSubViews()
+        setAutoLayout()
+        setExplainLabel()
     }
   
  
@@ -37,8 +77,12 @@ class HomeVC: UIViewController{
 extension HomeVC {
     private func addSubViews(){
         self.view.addSubview(explainLabel)
+        self.view.addSubview(addressLabel)
+        self.view.addSubview(addressStackView)
+        addressStackView.addArrangedSubview(addressTextField)
+        addressStackView.addArrangedSubview(addressButton)
         self.view.addSubview(mapView)
-        
+        self.view.addSubview(rentalButton)
     }
     private func setAutoLayout(){
         explainLabel.snp.makeConstraints { make in
@@ -46,14 +90,27 @@ extension HomeVC {
             make.left.equalToSuperview().offset(15)
             make.right.equalToSuperview().offset(-15)
         }
-        mapView.snp.makeConstraints { make in
+        addressLabel.snp.makeConstraints { make in
             make.top.equalTo(explainLabel.snp.bottom).offset(15)
             make.left.equalToSuperview().offset(15)
             make.right.equalToSuperview().offset(-15)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-15)
-            
         }
-        
+        addressStackView.snp.makeConstraints { make in
+            make.top.equalTo(addressLabel.snp.bottom).offset(15)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+        }
+        mapView.snp.makeConstraints { make in
+            make.top.equalTo(addressStackView.snp.bottom).offset(15)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+        }
+        rentalButton.snp.makeConstraints { make in
+            make.top.equalTo(mapView.snp.bottom).offset(15)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-15)
+        }
     }
     private func setExplainLabel(){ // explainLabel 설정
         let text = "가까운 곳의 킥보드를 찾아보세요.\n대여를 원하는 킥보드를 클릭하세요."
