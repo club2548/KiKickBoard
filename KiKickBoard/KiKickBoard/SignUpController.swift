f5c738b19f6936391f4160509a35baf589960eeb
gyeongeun <club2548@gmail.com>
2024-01-16 16:59:38 +0900

    Feat : save userInfo as UserDefaults SignUpVC에서 textField에 입력한 userInfo를 UserDefaults로 저장 기존 UserDefault와 일치하는 정보(ID, eMail, Mobile)가 있을 때, Alert 팝업

---
+++ KiKickBoard/KiKickBoard/SignUpController.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpController.swift
+//  KiKickBoard
+//
+//  Created by t2023-m0026 on 1/16/24.
+//
+
+import Foundation
---
+++ KiKickBoard/KiKickBoard/SignUpModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpModel.swift
+//  KiKickBoard
+//
+//  Created by t2023-m0026 on 1/16/24.
+//
+
+import Foundation
